87c5647b1d7003345b0f7ebd9fab23142a110709
EmmaKateCostanza <ecostanza8@gatech.edu>
2021-09-13 02:44:45 -0400

    Initial Commit

---
+++ PerfectRomComFinal/PerfectRomComFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectRomComFinalApp.swift
+//  PerfectRomComFinal
+//
+//  Created by Emma Kate Costanza on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct PerfectRomComFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
